dff077077884e2b017e3c2b7b0181ee31539f936
Sean Ross <seanmross9@gmail.com>
2017-06-22 02:11:51 -0400

    Progress on user story 5

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,14 @@
 require 'random_data'
 
-# # Create Users
-# 5.times do
-#   User.create!(
-#   email:    RandomData.random_email,
-#   password: RandomData.random_sentence,
-#   )
-# end
+# Create Users
+5.times do
+  User.create!(
+  email:    Faker::Internet.unique.email,
+  password: 'password',
+  password_confirmation: 'password',
+  confirmed_at: Time.at(0)
+  )
+end
 
 # Create custom user
 User.create!(
@@ -14,7 +16,7 @@ email:    'seanmross9@gmail.com',
 password: 'password',
 password_confirmation: 'password',
 confirmed_at: Time.at(0),
-role: 'standard',
+role: 'admin',
 )
 
 users = User.all
@@ -23,7 +25,7 @@ users = User.all
 # Create Wikis
 50.times do
   Wiki.create!(
-    #user:   users.sample,
+    user:   users.sample,
     title:  RandomData.random_sentence,
     body:   RandomData.random_paragraph
   )
